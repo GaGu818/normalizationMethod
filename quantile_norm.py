fb9aebb2ad3504a552893fc2f5529a4ec8ad008e
Yue <57375372+GaGu818@users.noreply.github.com>
2022-12-23 15:03:29 +0800

    Update quantile_norm.py

--- quantile_norm.py
+++ quantile_norm.py
@@ -24,17 +24,7 @@ def quantile_normalize(df):
 
 dir1 = 'quantile'
 norm="new"
-# data = pd.read_csv('../data/'+dir1+'.csv')
 
-# norm='QuantileNorm'
-# del data['ID']
-# # data = (data-data.min())/(data.max()-data.min())
-# y = data['condition'].values
-# del data['condition']
-# # x = data.values
-# print(data)
-# qn_feature=quantile_normalize(data)
-# print(qn_feature)
 qn_feature=pd.read_csv('../data/data_mapping/14F/newquantile.csv')
 sns.boxplot(data=qn_feature)
 sns.stripplot(data = qn_feature,size=1)
@@ -46,12 +36,5 @@ plt.ylabel("Measurement", size=18)
 plt.title("Boxplot of raw data after Quantile Normalization")
 plt.savefig('../data/data_mapping/14F/'+dir1+'/'+norm+'/Boxplot_after_Quantile_Normalization.png',dpi=150)
 
-# sns.boxplot(data=qn_feature)
-# # set x-axis label
-# plt.xlabel("Samples", size=18)
-# # set y-axis label
-# plt.ylabel("Measurement", size=18)
-# plt.title("Boxplot after Quantile Normalization")
-# plt.savefig('../data/'+dir1+'/'+norm+'/Boxplot_after_Quantile_Normalization_Seaborn.png',dpi=150)
 
 
